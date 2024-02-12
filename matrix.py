ed9c288fdb910b4cbb807691622b6ed3c5b4705c
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-12 13:20:00 +0100

    Matrix

--- matrix.py
+++ matrix.py
@@ -1,3 +1,10 @@
 import os 
 import pygame as pg
 # from randomm import choice, randrange
+
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+RES = WIDTH, HEIGHT =1600, 900
+FONT_SIZE = 40
+
+pg.init()
+surface = pg
