fde0a4503553f06b9b286390df3df89eb3516b47
{G} gurraoptimus <63198393+gurraoptimus@users.noreply.github.com>
2024-02-12 13:48:55 +0100

    Matrix

--- matrix.py
+++ matrix.py
@@ -11,7 +11,7 @@ surface = pg.display.set_mode(RES)
 clock = pg.time.Clock()
 
 katakana =[chr(int('0x30a0', 16) + i)for i in range(96)]
-
+font =
 while True:
     surface.fill(pg.Color('black'))
 
